f9c4a469e3586bb856e2eb997086610b69b7684b
Odisseuss <rares_lungescu@yahoo.com>
2020-04-06 21:08:49 +0300

    Add body-parser for accepting x-www-form-urlencoded and json data

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,20 @@
-require("body-parser");
+const bodyParser = require("body-parser");
 const express = require("express");
+const AddDataToDb = require("./AddDataToDb");
 require("dotenv").config();
 const { Client } = require("pg");
 const app = express();
-
+const db_connection_params = {
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+};
 const port = 1234;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Add database connection
 
@@ -16,18 +26,23 @@ const port = 1234;
 
 // Some other routes
 app.get("/", (req, res) => {
-  const client = new Client({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
-  });
+  const client = new Client({ ...db_connection_params });
   client.connect();
   client.query("SELECT * FROM datatable", (err, db_res) => {
-    res.send(db_res);
+    res.send(db_res.rows);
     client.end();
   });
+});
+
+app.post("/test", (req, res) => {
+  AddDataToDb(
+    req.body.id,
+    req.body.temperature,
+    req.body.humidity,
+    req.body.pressure,
+    req.body.gas
+  );
+  res.send("Request processed successfully");
 });
 
 app.listen(port, () => console.log(`App started at http://localhost:${port}/`));
