857de84400e56f80738cc0bfdda9375002c8a895
Rares Lungescu <rares_lungescu@yahoo.com>
2020-04-08 22:10:52 +0300

    Update script to execute multiple insertion queries

--- Backend/AddDataToDb.js
+++ Backend/AddDataToDb.js
@@ -8,21 +8,35 @@ const db_connection_params = {
   port: process.env.PG_PORT,
 };
 
-function AddToDb(timestamp, temperature, humidity, pressure, air_quality) {
+function AddToDb(
+  timestamp,
+  temperature,
+  humidity,
+  pressure,
+  air_quality_score
+) {
   const client = new Client({ ...db_connection_params });
   client.connect();
-  const query_values = [...arguments];
-  const insert_query = `INSERT INTO temperature(ts_collection_time, i_temperature_value) VALUES($1, $2);
-    INSERT INTO humidity(ts_collection_time, i_humidity_value) VALUES($1, $3);
-    INSERT INTO pressure(ts_collection_time, i_pressure_value) VALUES($1, $4);
-    INSERT INTO air_quality(ts_collection_time, i_aq_value) VALUES($1, $5);`;
-  client.query(insert_query, query_values, (err, db_res) => {
-    if (err) {
-      console.log("AddToDb err: \n", err.stack);
-    } else {
-      console.log(db_res.rows);
-    }
-    client.end();
+  const query_values = [
+    [timestamp, temperature],
+    [timestamp, humidity],
+    [timestamp, pressure],
+    [timestamp, air_quality_score],
+  ];
+  const insert_queries = [
+    `INSERT INTO temperature(ts_collection_time, i_temperature_value) VALUES($1, $2);`,
+    `INSERT INTO humidity(ts_collection_time, i_humidity_value) VALUES($1, $2);`,
+    `INSERT INTO pressure(ts_collection_time, i_pressure_value) VALUES($1, $2);`,
+    `INSERT INTO air_quality(ts_collection_time, i_aq_value) VALUES($1, $2);`,
+  ];
+  insert_queries.forEach((query, index) => {
+    client.query(query, query_values[index], (err, db_res) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(db_res.rows);
+      }
+    });
   });
 }
 module.exports = AddToDb;
