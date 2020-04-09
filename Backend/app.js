2ec4384966936a9af678f058d0ee19245cef97c8
Rares Lungescu <rares_lungescu@yahoo.com>
2020-04-09 21:43:23 +0300

    Send status code

--- Backend/app.js
+++ Backend/app.js
@@ -26,12 +26,11 @@ app.get("/", (req, res) => {
   const select_all_join_on_time =
     "SELECT temperature.ts_collection_time, i_temperature_value, i_humidity_value, i_pressure_value, i_aq_value FROM temperature, humidity, pressure, air_quality WHERE temperature.ts_collection_time = humidity.ts_collection_time AND humidity.ts_collection_time = pressure.ts_collection_time AND pressure.ts_collection_time = air_quality.ts_collection_time";
   client.query(select_all_join_on_time, (err, db_res) => {
-    console.log(db_res.rows[0].ts_collection_time);
-    res.send(db_res.rows);
+    res.status(200).send(db_res.rows);
     client.end();
   });
 });
 
-app.listen(process.env.SERVER_PORT, () =>
-  console.log(`App started at http://localhost:${process.env.SERVER_PORT}/`)
+app.listen(process.env.APP_PORT, () =>
+  console.log(`App started at http://localhost:${process.env.APP_PORT}/`)
 );
