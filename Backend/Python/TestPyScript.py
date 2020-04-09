fb36da38804d4cd75cc989af461b4176c229d7d8
Rares Lungescu <rares_lungescu@yahoo.com>
2020-04-09 17:51:27 +0300

    Update test script to give ascending time values

--- Backend/Python/TestPyScript.py
+++ Backend/Python/TestPyScript.py
@@ -1,5 +1,6 @@
 import time
-
+i = 36
 while True:
-    print("1999-01-08 04:05:30, 30 C, 1083 hPa, 60 %RH, 40")
+    print("1999-01-08 04:05:{0}, 30 C, 1083 hPa, 60 %RH, 40".format(i))
+    i = i + 1
     time.sleep(5)
