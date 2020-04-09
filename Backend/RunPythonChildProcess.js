82534f26477df7556a7cdb8abe237e7bc61656ee
Rares Lungescu <rares_lungescu@yahoo.com>
2020-04-09 17:50:54 +0300

    Update array indexes]

--- Backend/RunPythonChildProcess.js
+++ Backend/RunPythonChildProcess.js
@@ -15,7 +15,7 @@ pythonShell.on("message", function (msg) {
   regex_results = regex_results.slice(1);
   AddToDb(...regex_results);
   console.log(
-    `Timestamp: ${regex_results[1]}, Temperature: ${regex_results[2]}, Pressure: ${regex_results[3]}, Humidity: ${regex_results[4]}, Air Quality: ${regex_results[5]}`
+    `Timestamp: ${regex_results[0]}, Temperature: ${regex_results[1]}, Pressure: ${regex_results[2]}, Humidity: ${regex_results[3]}, Air Quality: ${regex_results[4]}`
   );
 });
 
