d4e7871c64a1bb04832e624ec06a7109ce260651
Rares Lungescu <rares_lungescu@yahoo.com>
2020-04-09 21:41:50 +0300

    Add second python child process

--- Backend/RunPythonChildProcess.js
+++ Backend/RunPythonChildProcess.js
@@ -4,11 +4,32 @@ const AddToDb = require("./AddDataToDb");
 const options = {
   scriptPath: "./Backend/Python/",
   pythonOptions: ["-u"],
+  args: [
+    process.env.PG_HOST,
+    process.env.PG_DATABASE,
+    process.env.PG_USER,
+    process.env.PG_PASSWORD,
+    process.env.PG_PORT,
+  ],
 };
 
 let regex = /(\d+-\d+-\d+\D\d+:\d+:\d+)\D*(\d+)\D*(\d+)\D*(\d+)\D*(\d+)/;
 
 let pythonShell = new PythonShell("TestPyScript.py", options);
+
+setTimeout(() => {
+  let pythonShell2 = new PythonShell("ComputeAverages.py", {
+    scriptPath: "./Backend/Python/",
+    pythonOptions: ["-u"],
+    args: [
+      process.env.PG_HOST,
+      process.env.PG_DATABASE,
+      process.env.PG_USER,
+      process.env.PG_PASSWORD,
+      process.env.PG_PORT,
+    ],
+  });
+}, 300000);
 
 pythonShell.on("message", function (msg) {
   let regex_results = msg.match(regex);
