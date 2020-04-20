e4f2a7bbd11a574c31fedf55cb32ce67465bdaf4
Rares Lungescu <rares_lungescu@yahoo.com>
2020-04-20 20:11:11 +0300

    Add dependencies to install script

--- ShellScripts/InstallDependencies.sh
+++ ShellScripts/InstallDependencies.sh
@@ -1,4 +1,4 @@
 sudo apt-get update
 sudo apt-get install pg libpq-dev
-sudo pip install psycopg2
+sudo pip install psycopg2 numpy
 bash CreateDbFromShell.sh
--- ShellScripts/install.sh
+++ ShellScripts/install.sh
@@ -1,1 +1,4 @@
+sudo apt-get updtate
+sudo apt-get upgrade
+sudo apt-get install postgersql
 sudo -H -u postgres bash InstallDependencies.sh
