b1e6e49dfc042beb60683ca817b3aa67c58e5112
Rares Lungescu <rares_lungescu@yahoo.com>
2020-04-21 16:57:20 +0300

    Create systemd startup config file, add autorun instructions to install script

--- ShellScripts/install.sh
+++ ShellScripts/install.sh
@@ -2,3 +2,10 @@ sudo apt-get updtate
 sudo apt-get upgrade
 sudo apt-get install postgersql
 sudo -H -u postgres bash InstallDependencies.sh
+
+
+# Copy the systemd server autorun config file, enable and activate the service
+sudo cp piweatherstation.service /etc/systemd/system/
+sudo chmod u+rwx /etc/systemd/system/piweatherstation.service
+sudo systemctl enable piweatherstation.service
+sudo systemctl start piweatherstation.service
