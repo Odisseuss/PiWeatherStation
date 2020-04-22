dbede1a5c6ce35be0b85d15ccb63de95bd95208e
Rares Lungescu <rares_lungescu@yahoo.com>
2020-04-22 23:17:17 +0300

    Add comments to bash scripts

--- ShellScripts/install.sh
+++ ShellScripts/install.sh
@@ -1,9 +1,11 @@
+# Set up the environment
 sudo apt-get updtate
 sudo apt-get upgrade
+# Install dependencies
 sudo apt-get install postgersql
 sudo -H -u postgres bash InstallDependencies.sh
 
-
+# Configure persistent server
 # Copy the systemd server autorun config file, enable and activate the service
 sudo cp piweatherstation.service /etc/systemd/system/
 sudo chmod u+rwx /etc/systemd/system/piweatherstation.service
