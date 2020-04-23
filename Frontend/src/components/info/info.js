6c9ae24cbabd4a74eb237ddfedef6ddfe737e7d3
Rares Lungescu <rares_lungescu@yahoo.com>
2020-04-23 23:57:24 +0300

    Fix Kaur's bullshit

---
+++ Frontend/src/components/info/info.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './info.css'
+
+
+export default function info() {
+    return (
+        
+        <section>
+            <h1>Weather App Interface</h1>
+            <hr />
+            <p>Created by: Kaur Kadak, Pavel Dobrev, Rares Lungescu, Ahmed Aidarous, Aris Christou, Vlad Velicu</p>
+        </section>
+    )
+}
---
+++ Frontend/src/components/navigationBar.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import {Nav, Navbar} from 'react-bootstrap';
+import {BrowserRouter, Link, NavLink, Redirect, Route, Switch} from 'react-router-dom'
+import PastData from './pastDataPage'
+import HomePage from './homepage'
+import "./navigationBar.css"
+
+
+
+function navigationBar(props){
+    
+    return (
+
+  
+        <Navbar expand='lg'>
+            
+            <Navbar.Brand as={Link} to="/">Weather App</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Nav className="mr-auto">
+              <Nav.Link as={NavLink} to="/pastData">Past Data</Nav.Link>
+              
+            </Nav>
+        </Navbar>
+  
+                   
+    )
+ 
+    
+}
+export default navigationBar;
