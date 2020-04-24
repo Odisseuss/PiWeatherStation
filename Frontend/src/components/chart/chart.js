64ccdc2c163b444b861d1f97ba3164c3eed80003
Rares Lungescu <rares_lungescu@yahoo.com>
2020-04-24 08:59:33 +0300

    hardcodeala

--- Frontend/src/components/chart/chart.js
+++ Frontend/src/components/chart/chart.js
@@ -2,31 +2,47 @@ import React, { Component } from "react";
 import { Line } from "react-chartjs-2";
 import "./chart.css";
 
-const Chart = (props) => {
-  let chart_data = {
-    labels: [
-      "10.00",
-      "10.30",
-      "11.00",
-      "11.30",
-      "12.00",
-      "12.30",
-      "13.00",
-      "13.30",
-    ],
-    datasets: [
-      {
-        label: "Temperature",
-        data: [props.data.temperature],
+class chart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chartData: {
+        labels: [
+          "19/04/2020",
+          "20/04/2020",
+          "21/04/2020",
+          "22/04/2020",
+          "23/04/2020",
+        ],
+        datasets: [
+          {
+            label: "Temperature",
+            data: [21, 21, 26, 23, 17],
+          },
+          {
+            label: "Humidity",
+            data: [40, 36, 69, 45, 42],
+          },
+          {
+            label: "Pressure",
+            data: [1012, 1013, 1015, 1012, 1012],
+          },
+          {
+            label: "Air Quality",
+            data: [3, 4, 5, 2, 6, 3, 2, 9],
+          },
+        ],
       },
-    ],
-  };
-  console.log(chart_data);
-  return (
-    <div className="chart">
-      <Line data={chart_data} width={750} height={300} />
-    </div>
-  );
-};
+    };
+  }
 
-export default Chart;
+  render() {
+    return (
+      <div className="chart">
+        <Line data={this.state.chartData} width={750} height={300} />
+      </div>
+    );
+  }
+}
+
+export default chart;
